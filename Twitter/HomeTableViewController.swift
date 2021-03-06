a58a817481047967a4b22cfcc02b86ed64386f28
jonriv48376 <58375544+jonriv48376@users.noreply.github.com>
2021-03-06 01:15:53 -0500

    Part 1

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,97 @@
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
+    let myRefreshControl = UIRefreshControl()
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+    }
+    
+    @objc func loadTweets(){
+            
+        numberOfTweet = 20
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": 20]
+            
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for tweet in tweets{
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+        }, failure: { (Error) in
+            print("Could not retrieve tweets!")
+        })
+    }
+    
+    func loadMoreTweets(){
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json" 
+        numberOfTweet += 20
+        let myParams = ["count": numberOfTweet]
+
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for tweet in tweets{
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+        }, failure: { (Error) in
+            print("Could not retrieve tweets!")
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == tweetArray.count{
+            loadMoreTweets()
+        }
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+            
+            TwitterAPICaller.client?.logout()
+            self.dismiss(animated: true, completion: nil)
+            UserDefaults.standard.set(false, forKey: "userLoggedIn")
+        }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetTableViewCell
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let imageUrlString = imageUrl?.absoluteString
+        let newImageUrlString = (imageUrlString?.replacingOccurrences(of: "_normal", with: ""))! //getting orginal profile picture quality instead of normal
+        let newImageUrl = URL(string: newImageUrlString)
+        let data = try? Data(contentsOf: newImageUrl!)
+        
+        if let imageData = data{
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        
+        return cell
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweetArray.count
+    }
+
+}
---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Jonathan Rivero on 3/5/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
